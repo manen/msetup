252a89989f054d946c0801e8fe43ab0093c59984
manen <manen@manen.me>
2022-03-06 12:33:31 +0100

    Enable symlinking the executable it's like installing it but with extra steps

--- cmd.v
+++ cmd.v
@@ -3,6 +3,10 @@ module main
 import os
 import cli
 
+const (
+	symlink_path = '/usr/bin/msetup'
+)
+
 fn main() {
 	mut app := cli.Command{
 		name: 'msetup'
@@ -16,9 +20,27 @@ fn main() {
 			// 		return
 			// 	}
 			// },
+			cli.Command{
+				name: 'symlink'
+				description: 'Symlink the executable into \$PATH (hopefully)'
+				execute: symlink
+			},
+			cli.Command{
+				name: 'unlink'
+				description: 'Deletes the msetup symlink from \$PATH'
+				execute: unlink
+			},
 		]
 	}
 
 	app.setup()
 	app.parse(os.args)
+}
+
+fn symlink(cmd cli.Command) ? {
+	os.symlink(os.executable(), symlink_path) ?
+}
+
+fn unlink(cmd cli.Command) ? {
+	os.rm(symlink_path) ?
 }
